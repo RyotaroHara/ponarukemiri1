3f3dd112eda9ac915de5c027810468b069c08357
risako.hamba <risako.hamba@rakuten.com>
2018-07-09 05:13:19 +0000

    modified

--- routes/web.php
+++ routes/web.php
@@ -13,14 +13,6 @@
 
 Route::get('/', 'WelcomeController@index');
 
-//Exercise 
-Route::get('Exercise/cafeteria', 'ExerciseController@cafeteria')->name('Exercise.cafeteria');
-Route::get('Exercise/way', 'ExerciseController@way')->name('Exercise.way');
-Route::get('Exercise/office', 'ExerciseController@office')->name('Exercise.office');
-
-//fitnessgoods 
-Route::get('fitnessgoods/ichiba', 'FitnessgoodsController@ichiba')->name('fitnessgoods.ichiba');
-
 // User registration
 Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
 Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
@@ -31,7 +23,7 @@ Route::post('login', 'Auth\LoginController@login')->name('login.post');
 Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
 
 Route::group(['middleware' => ['auth']], function () {
-    Route::resource('users', 'UsersController', ['only' => ['show']]);
+Route::resource('users', 'UsersController', ['only' => ['show']]);
 
     
 });
