8aa9c646c3f898ca638fdba39d5f66f48899016f
nana.fukumoto <naaaaachi1101@gmail.com>
2018-07-09 06:51:12 +0000

    nanimoyattenai

--- resources/views/Exercise/cafeteria.blade.php
+++ resources/views/Exercise/cafeteria.blade.php
@@ -3,4 +3,6 @@
 @section('content')
     <h1>カフェテリア</h1>
 
+<a href="http://928bcb987a8146c1be58a3319ad7d4b7.vfs.cloud9.us-east-2.amazonaws.com/Exercise/office"><img src="https://www.homepage-tukurikata.com/image/hoge.jpg" alt="ほほほ" width="233" height="165" border="0" /></a>
+
 @endsection
