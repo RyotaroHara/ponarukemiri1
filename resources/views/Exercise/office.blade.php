e6ebf5316c1b456192385fe4933d9aca236f7e18
ryotaro.hara <ryotaro.hara@rakuten.com>
2018-07-10 01:44:29 +0000

    little bit change in Office

--- resources/views/Exercise/office.blade.php
+++ resources/views/Exercise/office.blade.php
@@ -3,15 +3,16 @@
 @section('content')
     <h1>OFFICE</h1>
     @if (Auth::check())
-                        <li><a href="{{ route('office.office1') }}">オフィス1</a></li>
-                        <li><a href="{{ route('office.office2') }}">オフィス2</a></li>
-                        <li><a href="{{ route('office.office3') }}">オフィス3</a></li>
-                        <li><a href="{{ route('office.office4') }}">オフィス4</a></li>
-                        <li><a href="{{ route('office.office5') }}">オフィス5</a></li>
-                        <li><a href="{{ route('office.office6') }}">オフィス6</a></li>
-                        <li><a href="{{ route('office.office7') }}">オフィス7</a></li>
-                        <li><a href="{{ route('office.office8') }}">オフィス8</a></li>
-
+                        <li><a href="{{ route('office.office1') }}">小顔になるエクササイズ</a></li>
+                        <li><a href="{{ route('office.office2') }}">ふくらはぎの引き締め</a></li>
+                        <li><a href="{{ route('office.office3') }}">ふとももの引き締め</a></li>
+                        <li><a href="{{ route('office.office4') }}">ヒップアップ</a></li>
+                        <li><a href="{{ route('office.office5') }}">ウエストの引き締め</a></li>
+                        <li><a href="{{ route('office.office6') }}">二の腕の引き締め</a></li>
+                        <li><a href="{{ route('office.office7') }}">空気椅子</a></li>
+                        <li><a href="{{ route('office.office8') }}">肩のストレッチ</a></li>
+                        <li><a href="{{ route('office.office9') }}">頭のマッサージ</a></li>
+                        <li><a href="{{ route('office.office10') }}">腹式呼吸</a></li>
 
     @else
                         <li><a>このでぶが！</a></li>
--- resources/views/office/office2.blade.php
+++ resources/views/office/office2.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Office2</h1>
-
+    <h1>ふくらはぎの引き締め</h1>
+<p>力を入れながらつま先をゆっくり挙げて5秒キープ、かかとを挙げて5秒キープを繰り返しましょう</p>
+<p>むくみ防止効果があります。</p>
 @endsection
