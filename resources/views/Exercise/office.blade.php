afe6e1da00058908bf294f038dceeb12dc63406e
shohei.imamura <shoboc075@gmail.com>
2018-07-10 05:29:38 +0000

    exercise content

--- resources/views/Exercise/office.blade.php
+++ resources/views/Exercise/office.blade.php
@@ -1,9 +1,30 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>OFFICE</h1>
-    @if (Auth::check())
-                        <li><a href="{{ route('office.office1') }}">小顔になるエクササイズ</a></li>
+
+
+<div class="row">
+  <div class="col-4 col-md-1">
+        <div class=a>
+            @include('Exercise.commons.office')
+            @include('Exercise.commons.way')
+            @include('Exercise.commons.cafeteria')
+        </div>
+  </div>
+  <div class="col-7 col-md-5">
+        <div style="padding:10px;width:450px;height:570px;background:#DDDDDD;">
+            @include('Exercise.Genre.office')
+        </div>
+  </div>
+  
+  <div class="col-7 col-md-6">
+        <div style="padding:10px;width:700px;height:570px;background:#ced;border:1px solid #a7e;">
+            とばない
+        </div>
+            
+  </div>
+    
+                        <li><a href="{{ route('office.office1') }}">小顔エクササイズ</a></li>
                         <li><a href="{{ route('office.office2') }}">ふくらはぎの引き締め</a></li>
                         <li><a href="{{ route('office.office3') }}">ふとももの引き締め</a></li>
                         <li><a href="{{ route('office.office4') }}">ヒップアップ</a></li>
@@ -14,7 +35,5 @@
                         <li><a href="{{ route('office.office9') }}">頭のマッサージ</a></li>
                         <li><a href="{{ route('office.office10') }}">腹式呼吸</a></li>
 
-    @else
-                        <li><a>このでぶが！</a></li>
-    @endif
+   
 @endsection
