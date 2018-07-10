7684bb579152126ee686f8b16171a9dd15eb7b01
nana.fukumoto <naaaaachi1101@gmail.com>
2018-07-10 05:51:15 +0000

    kayoubi

--- resources/views/Exercise/cafeteria.blade.php
+++ resources/views/Exercise/cafeteria.blade.php
@@ -18,6 +18,5 @@
                         <li><a>このでぶが！</a></li>
     @endif
 
-<a href="http://928bcb987a8146c1be58a3319ad7d4b7.vfs.cloud9.us-east-2.amazonaws.com/Exercise/office"><img src="https://www.homepage-tukurikata.com/image/hoge.jpg" alt="ほほほ" width="233" height="165" border="0" /></a>
 
 @endsection
