94973bd0be2e012cf5fcbeb27d14c10da59c9a68
shohei.imamura <shoboc075@gmail.com>
2018-07-10 03:28:04 +0000

    cafeteria blade 編集

---
+++ resources/views/Exercise/Genre/office.blade.php
@@ -0,0 +1,44 @@
+
+<div class="card" style="width: 45rem;">
+  <img class="card-img-top" src="{{ asset('images/viber image.jpg') }}" alt="Card image cap" style="width:430px;">
+  
+  <div class="card-body">
+     <div class="row">
+            <table class="table table-borderless" style="margin-left:25px; margin-top:10px;">
+    
+                <tr>
+                    <td><button type="button" class="btn btn-light" style="background-color:#EEEEEE;">Primary</button></td>
+                    <td><button type="button" class="btn btn-outline-primary" style="background-color:#EEEEEE";>Primary</button></td>
+                    <td><button type="button" class="btn btn-outline-primary" style="background-color:#EEEEEE;">Primary</button></td>
+                </tr>
+    
+                <tr>
+                    <td><button type="button" class="btn btn-light" style="background-color:#EEEEEE;">Primary</button></td>
+                    <td><button type="button" class="btn btn-outline-primary" style="background-color:#EEEEEE";>Primary</button></td>
+                    <td><button type="button" class="btn btn-outline-primary" style="background-color:#EEEEEE;">Primary</button></td>
+                </tr>
+    
+                <tr>
+                    <td><button type="button" class="btn btn-light" style="background-color:#EEEEEE;">Primary</button></td>
+                    <td><button type="button" class="btn btn-outline-primary" style="background-color:#EEEEEE";>Primary</button></td>
+                    <td><button type="button" class="btn btn-outline-primary" style="background-color:#EEEEEE;">Primary</button></td>
+                </tr>
+                
+                <tr>
+                    <td><button type="button" class="btn btn-light" style="background-color:#EEEEEE;">Primary</button></td>
+                    <td><button type="button" class="btn btn-outline-primary" style="background-color:#EEEEEE";>Primary</button></td>
+                    <td><button type="button" class="btn btn-outline-primary" style="background-color:#EEEEEE;">Primary</button></td>
+                </tr>
+                
+                <tr>
+                    <td><button type="button" class="btn btn-light" style="background-color:#EEEEEE;">Primary</button></td>
+                    <td><button type="button" class="btn btn-outline-primary" style="background-color:#EEEEEE";>Primary</button></td>
+                    <td><button type="button" class="btn btn-outline-primary" style="background-color:#EEEEEE;">Primary</button></td>
+                </tr>
+                
+                
+            </table>
+            
+     </div>
+  </div>
+</div>
