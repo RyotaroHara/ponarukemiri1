b2834de6a6aa4380142c4a46723288be7b989be8
risako.hamba <risako.hamba@rakuten.com>
2018-07-11 01:57:28 +0000

    edit office.blade

--- resources/views/Exercise/Genre/office.blade.php
+++ resources/views/Exercise/Genre/office.blade.php
@@ -9,11 +9,14 @@
                 <tr>
                     <td style="border:none;"><button type="button" class="btn btn-light" style="background-color:#EEEEEE;"><a href="{{ route('office.office1') }}">小顔Exercise</a></button></td>
                     <td style="border:none;"><button type="button" class="btn btn-outline-primary" style="background-color:#EEEEEE";><a href="{{ route('office.office2') }}">ふくらはぎ引き締め</a></button></td>
-                    <td style="border:none;"><button type="button" class="btn btn-outline-primary" style="background-color:#EEEEEE;"><a href="{{ route('office.office3') }}">ふともも引き締め</a></button></td>
                 </tr>
     
                 <tr>
+                    <td style="border:none;"><button type="button" class="btn btn-outline-primary" style="background-color:#EEEEEE;"><a href="{{ route('office.office3') }}">ふともも引き締め</a></button></td>
                     <td style="border:none;"><button type="button" class="btn btn-light" style="background-color:#EEEEEE;"><a href="{{ route('office.office4') }}">ヒップアップ</a></button></td>
+                </tr>
+                
+                <tr>
                     <td style="border:none;"><button type="button" class="btn btn-outline-primary" style="background-color:#EEEEEE";><a href="{{ route('office.office5') }}">ウエストの引き締め</a></button></td>
                     <td style="border:none;"><button type="button" class="btn btn-outline-primary" style="background-color:#EEEEEE;"><a href="{{ route('office.office6') }}">二の腕の引き締め</a></button></td>
                 </tr>
@@ -21,21 +24,12 @@
                 <tr>
                     <td style="border:none;"><button type="button" class="btn btn-light" style="background-color:#EEEEEE;"><a href="{{ route('office.office7') }}">空気椅子</a></button></td>
                     <td style="border:none;"><button type="button" class="btn btn-outline-primary" style="background-color:#EEEEEE";><a href="{{ route('office.office8') }}">肩のストレッチ</a></button></td>
+                </tr>
+                
+                <tr>
                     <td style="border:none;"><button type="button" class="btn btn-outline-primary" style="background-color:#EEEEEE;"><a href="{{ route('office.office9') }}">頭のマッサージ</a></button></td>
-                </tr>
-                
-                <tr>
                     <td style="border:none;"><button type="button" class="btn btn-light" style="background-color:#EEEEEE;"><a href="{{ route('office.office10') }}">腹式呼吸</a></button></td>
-                    <td style="border:none;"><button type="button" class="btn btn-outline-primary" style="background-color:#EEEEEE";>Primary</button></td>
-                    <td style="border:none;"><button type="button" class="btn btn-outline-primary" style="background-color:#EEEEEE;">Primary</button></td>
                 </tr>
-                
-                <tr>
-                    <td style="border:none;"><button type="button" class="btn btn-light" style="background-color:#EEEEEE;">Primary</button></td>
-                    <td style="border:none;"><button type="button" class="btn btn-outline-primary" style="background-color:#EEEEEE";>Primary</button></td>
-                    <td style="border:none;"><button type="button" class="btn btn-outline-primary" style="background-color:#EEEEEE;">Primary</button></td>
-                </tr>
-                
                 
             </table>
             
