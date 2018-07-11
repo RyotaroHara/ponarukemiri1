ab7fd19c6352574ecf8b2b356c6e8ae6901161dc
shohei.imamura <shoboc075@gmail.com>
2018-07-11 06:34:57 +0000

    change footer link

--- resources/views/commons/footer.blade.php
+++ resources/views/commons/footer.blade.php
@@ -1,10 +1,12 @@
-<div style="margin-left:-200px;margin-top:20px;
-            text-align:center;line-height:40px;
-            width:1535px;height:45px;background:#DDDDDD;">
+<div style="margin-top:20px;
+            min-width:1000px;height:45px;background:#DDDDDD;">
+
+    <div class=b style="text-align:center;margin-top:20px;"><h3>Battle</h3></div>
+
+    <div class=b style="float:right;margin-top:-20px;margin-right:15px;"><a href="{{ route('users.show', Auth::user()->id) }}">MyPage</div>
     
-   
-    
-   linkで飛ばす
+    <div class=b style="float:left;margin-top:-20px;margin-left:15px;"><a href="{{ route('Exercise.office') }}">Exercise</a></div>
+
 
 
 </div>
