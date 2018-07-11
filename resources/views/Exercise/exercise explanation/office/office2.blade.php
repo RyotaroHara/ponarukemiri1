3a79096f5758f48de616371a617d47d2bf115218
shohei.imamura <shoboc075@gmail.com>
2018-07-11 04:45:09 +0000

    office,way view adjustment

--- resources/views/Exercise/exercise explanation/office/office2.blade.php
+++ resources/views/Exercise/exercise explanation/office/office2.blade.php
@@ -2,18 +2,19 @@
 
 @section('content')
 
+@if(Auth::check())
 
 <div class="row">
   <div class="col-4 col-md-1">
         <div class=a>
-            @include('Exercise.commons.present location.office')
-            @include('Exercise.commons.way')
-            @include('Exercise.commons.cafeteria')
+            @include('Exercise.tab.present location.P_tab_office')
+            @include('Exercise.tab.normal.tab_way')
+            @include('Exercise.tab.normal.tab_cafeteria')
         </div>
   </div>
   <div class="col-7 col-md-5">
         <div style="padding:10px;width:450px;height:570px;background:#DDDDDD;">
-            @include('Exercise.Genre.office')
+            @include('Exercise.link_to_exercise.links_office')
         </div>
   </div>
   
@@ -28,4 +29,7 @@
         </div>
             
   </div>
+  
+@endif
+
 @endsection
--- resources/views/Exercise/exercise explanation/way/way1.blade.php
+++ resources/views/Exercise/exercise explanation/way/way1.blade.php
@@ -2,18 +2,19 @@
 
 @section('content')
 
+@if (Auth::check())
 
 <div class="row">
   <div class="col-4 col-md-1">
         <div class=a>
-            @include('Exercise.commons.office')
-            @include('Exercise.commons.present location.way')
-            @include('Exercise.commons.cafeteria')
+            @include('Exercise.tab.normal.tab_office')
+            @include('Exercise.tab.present location.P_tab_way')
+            @include('Exercise.tab.normal.tab_cafeteria')
         </div>
   </div>
   <div class="col-7 col-md-5">
         <div style="padding:10px;width:450px;height:570px;background:#DDDDDD;">
-            @include('Exercise.Genre.way')
+            @include('Exercise.link_to_exercise.links_way')
         </div>
   </div>
   
@@ -28,5 +29,7 @@
             
   </div>
   
+@endif
+
   
 @endsection
