58b0a4b31e4c5965a21abc5cba2389da3644d65b
shohei.imamura <shoboc075@gmail.com>
2018-07-11 07:12:47 +0000

    category tab changed entire link

--- resources/views/Exercise/tab/normal/tab_cafeteria.blade.php
+++ resources/views/Exercise/tab/normal/tab_cafeteria.blade.php
@@ -1,4 +1,3 @@
-
-<div style="padding:30px;text-align:center;writing-mode:vertical-rl;width:80px;height:190px;background:#DDDDDD;">
-<a href="{{ route('Exercise.cafeteria') }}">カフェテリア</a>
-</div>
+<a href="{{ route('Exercise.cafeteria') }}" style="text-decoration:none;">
+<div class="tab_box">カフェテリア</div>    
+</a>
