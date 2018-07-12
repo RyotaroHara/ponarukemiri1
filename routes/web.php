8e779e4ad6837c6e364e84f39a9db6d1607a4d0a
ryotaro.hara <ryotaro.hara@rakuten.com>
2018-07-12 01:15:22 +0000

    add example of battle from tech

--- routes/web.php
+++ routes/web.php
@@ -51,6 +51,8 @@ Route::get('office/office8', 'ExerciseController@office8')->name('office.office8
 Route::get('office/office9', 'ExerciseController@office9')->name('office.office9');
 Route::get('office/office10', 'ExerciseController@office10')->name('office.office10');
 
+//battle page
+Route::get('battle/battle', 'BattleController@battle')->name('battle.battle');
 
 //fitnessgoods 
 Route::get('fitnessgoods/ichiba', 'FitnessgoodsController@ichiba')->name('fitnessgoods.ichiba');
