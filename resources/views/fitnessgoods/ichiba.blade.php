2338b67d2f8ecdf9cc4cb979c20e8d4ca3fd8d00
shohei.imamura <shoboc075@gmail.com>
2018-07-20 02:57:17 +0000

    fitnessgoods->battle

--- resources/views/fitnessgoods/ichiba.blade.php
+++ resources/views/fitnessgoods/ichiba.blade.php
@@ -1,6 +1,43 @@
 @extends('layouts.app')
-
 @section('content')
-    <h1>楽天市場</h1>
+@if(Auth::check())
 
+<div class="battle_background" alt="otameshi" >
+
+  <div class="container">
+    <div class="Hp">
+      <div class="col-sm-4">
+        One of three columns
+      </div>
+      <div class="col-sm-4">
+        One of three columns
+      </div>
+      <div class="col-sm-4">
+        One of three columns
+      </div>
+    </div>
+  </div>
+  
+  <?php
+  print $_SERVER['REQUEST_URI'];
+  ?>
+    
+  
+  
+    <div class="row align-items-end">
+      <div class="col-sm-8" style="text-align:center;">
+        One of three columns
+      </div>
+      <div class="col-sm-4">
+        One of three columns
+      </div>
+    
+    </div>
+  
+
+</div>
+
+
+
+@endif
 @endsection
