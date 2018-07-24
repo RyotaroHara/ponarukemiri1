42aeec2ec241e450e3549eb59142140084ad8b27
nana.fukumoto <naaaaachi1101@gmail.com>
2018-07-24 04:53:53 +0000

    hamba~~~~

--- resources/views/battle/basic page/cafeteria battle.blade.php
+++ resources/views/battle/basic page/cafeteria battle.blade.php
@@ -9,12 +9,7 @@
     <div class="container">
   <div class="row">
     <div class="col-lg-4">
-      1 of 2
-    </div>
-    <div class="col-lg-4">
-      
-      
-      <?php
+     <h1><?php
         $all_cal = 0;
         for ($i=1; $i<200; $i++) {
     
@@ -22,85 +17,113 @@
     $num = DB::table('battles')->where('user_id',"$user->id")->where('id',"$i")->value('num');
     $all_cal += $cal*$num;
 }
-
-
-$nokori_cal = floor((("{$user->Weight}"-"{$user->IdealWeight}")*7200)-$all_cal);
-$weight_sa = "{$user->Weight}"-"{$user->IdealWeight}";
-print $weight_sa."kg減量まであと".$nokori_cal."kcalだよ";
-print "<br />"."{$user->year}"."年"."{$user->month}"."月"."{$user->day}"."日までに結果が欲しい";
-                ?>
+    $level = floor($all_cal/((("{$user->Weight}"-"{$user->IdealWeight}")*7200)/100));
+    print "<br />今".$level."レベルだよ";
+    ?>  </h1>
                 
-                <html>
-
-<body>
+<div> IMAGE</div>
+                
+                
+                
 <form action="/battles" method="post">
-　  <div class="status text-left">　
-　  <a href = "#" class = "list-group-item active">エクササイズ選択</a>
-                <a href = "#" class = "list-group-item">
+　 <div class="status text-left ">　
+　   <div class = "list-group-item active">エクササイズ選択</div>
+        <div class = "list-group-item">
+        {!! Form::open(['route' => 'battles.store']) !!}
+        <input type="hidden" name="cal" value="{{ ($user->Weight)*0.0005 }}" >
+        <input type="number" name="num" value="" style="width:50px">回
+        {!! Form::submit('腕立て') !!}をした。 <br>
+        {!! Form::close() !!}</div>
+        <div class = "list-group-item">
+        {!! Form::open(['route' => 'battles.store']) !!}
+        <input type="hidden" name="cal" value="{{ ($user->Weight)*0.1 }}">
+        <input type="hidden" name="num" value="1">
+        {!! Form::submit('4階から9階まで階段であがる') !!} <br>
+        {!! Form::close() !!}
+        </div>
+        <div class = "list-group-item">
+        {!! Form::open(['route' => 'battles.store']) !!}
+        <input type="hidden" name="cal" value="{{ ($user->Weight)*0.0875 }}">
+        <input type="hidden" name="num" value="1">
+        {!! Form::submit('9階から4階まで階段でおりる') !!}<br>
+        {!! Form::close() !!}
+        </div>
+        <div class = "list-group-item">
+        {!! Form::open(['route' => 'battles.store']) !!}
+        <input type="hidden" name="cal" value="2.7">
+        <input type="number" name="num" value="" style="width:50px">分
+        {!! Form::submit('電車内でつま先立ち') !!}<br>
+        {!! Form::close() !!}
+        </div>
+        <div class = "list-group-item">
+        {!! Form::open(['route' => 'battles.store']) !!}    
+        <input type="hidden" name="cal" value="{{ ($user->Weight)*0.29 }}">
+        <input type="hidden" name="num" value="1">
+        {!! Form::submit('二子玉川駅からクリムゾンハウスまで歩く') !!}<br>
+        {!! Form::close() !!}
+        </div>
+        <div class = "list-group-item">
+        {!! Form::open(['route' => 'battles.store']) !!}    
+        <input type="number" name="cal" value="" style="width:60px">
+        <input type="hidden" name="num" value="1">kcal消費する
+        {!! Form::submit('運動をした') !!}<br>
+        {!! Form::close() !!}
+        </div>    
+      </div>
+    </div>  
+</form>
     
-    {!! Form::open(['route' => 'battles.store']) !!}
-    <input type="hidden" name="cal" value="{{ ($user->Weight)*0.0005 }}" >
-    <input type="number" name="num" value="" style="width:50px">回
-    {!! Form::submit('腕立て') !!}をした。 <br>
-    {!! Form::close() !!}</a>
-    <a href = "#" class = "list-group-item">
-    {!! Form::open(['route' => 'battles.store']) !!}
-    <input type="hidden" name="cal" value="{{ ($user->Weight)*0.1 }}">
-    <input type="hidden" name="num" value="1">
-    {!! Form::submit('4階から9階まで階段であがる') !!} <br>
-    {!! Form::close() !!}
-    </a>
-    <a href = "#" class = "list-group-item">
-    {!! Form::open(['route' => 'battles.store']) !!}
-    <input type="hidden" name="cal" value="{{ ($user->Weight)*0.0875 }}">
-    <input type="hidden" name="num" value="1">
-    {!! Form::submit('9階から4階まで階段でおりる') !!}<br>
-    {!! Form::close() !!}
-    </a>
-    <a href = "#" class = "list-group-item">
-    {!! Form::open(['route' => 'battles.store']) !!}
-    <input type="hidden" name="cal" value="2.7">
+    <div class="status text-right ">
+      
+      MONSTER KANA<br>
+      @if ($level > 25)
+      <img src="{{ url('images/fatman.jpg') }}"> 
+      @else
+      <img src="{{ url('images/fatman.jpg') }}">
+      @endif
+    
+    <br>
+    <div> 
+    のこりＨＰ
+         <meter min=0 max=100 value=<?php 
+$finish = strtotime("{$user->year}-{$user->month}-{$user->day}");
+$re_sa = $finish - time();
+$re_amari = $re_sa%(24*60*60);
+$re_hi = ($re_sa - $re_amari)/(24*60*60);
 
-    <input type="number" name="num" value="" style="width:50px">分
-    {!! Form::submit('電車内でつま先立ち') !!}<br>
+$re_day_cal_amari = (("{$user->Weight}"-"{$user->IdealWeight}")*7200 - $all_cal)%$re_hi;
+$re_day_cal = (("{$user->Weight}"-"{$user->IdealWeight}")*7200 - $all_cal - $re_day_cal_amari )/$re_hi;
 
-    {!! Form::close() !!}
-    </a>
-    <a href = "#" class = "list-group-item">
-    {!! Form::open(['route' => 'battles.store']) !!}    
-    <input type="hidden" name="cal" value="{{ ($user->Weight)*0.29 }}">
-    <input type="hidden" name="num" value="1">
-    {!! Form::submit('二子玉川駅からクリムゾンハウスまで歩く') !!}<br>
-    {!! Form::close() !!}
-    </a>
-    <a href = "#" class = "list-group-item">
-    {!! Form::open(['route' => 'battles.store']) !!}    
-    <input type="number" name="cal" value="" style="width:60px">
-    <input type="hidden" name="num" value="1">kcal消費する
-    {!! Form::submit('運動をした') !!}<br>
-    {!! Form::close() !!}
+$timestamp = time() ;
+$today = date( "Y-m-d" , $timestamp ) ;
+$sum_cal = 0;
+
+for ($i=1; $i<200; $i++) {
+
+    $cal = DB::table('battles')->where('user_id',"$user->id")->whereDate('created_at', '=', "$today")->where('id',"$i")->value('cal');
+    $num = DB::table('battles')->where('user_id',"$user->id")->whereDate('created_at', '=', "$today")->where('id',"$i")->value('num');
+    $sum_cal += $cal*$num;
+    }
     
-    </a>    
-    
+$damage = $sum_cal*100/$re_day_cal;
+      
+
+//echo $today;
+//今日消費したカロリー（$sum_cal）の計算　
+
+          print 100 - $damage;    ?> style="width: 200px;" low="20" high="80" optimum="90" >50%</meter>
+ <?php  print floor(100 - $damage) ?>/100
+    </div>
+    </div>        
+     
+        </div>
     </div>
 </form>
 </body>
 </html>
     </div>
   </div>
-  <div class="row">
-    <div class="col-lg-5" style="margin-top:360px;padding:10px; width:400px; height:220px; background-color:skyblue;">
-      
-     
-      
-      
-    </div>
-    <div class="col-lg-5" style="margin-top:360px;margin-left:10px;padding:10px; width:700px; height:220px; background-color:blue;">
-      2 of 3
-    </div>
-    
-  </div>
-</div>
+  
 
 @endif
 
