57e51b63fc7748d6a04902038c4640122053f348
EC2 Default User <ec2-user@ip-172-31-32-157.us-east-2.compute.internal>
2018-07-24 06:27:31 +0000

    top-page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,24 +1,25 @@
 @extends('layouts.app')
 @section('content')
 @section('cover')
-    <div class="cover">
+<div class="container">
+    <div class="cover"></div>
         <div class="cover-inner">
             <div class="cover-contents">
                 <!-- <h1>OFFICEで健康ルンルンルン</h1> -->
                 
  
-               
                 
                  @if (!Auth::check())
-                    <a href="{{ route('signup.get') }}" class="btn btn-success btn-lg" style="margin-left:0px;margin-top:450px;">
+                    <a href="{{ route('signup.get') }}" class="btn btn-success btn-lg">
                         Start Exercise</a>
                         
-                    <a href="{{ route('login') }}" class="btn btn-success btn-lg" style="margin-left:30px;margin-top:450px;">
+                    <a href="{{ route('login') }}" class="btn btn-success btn-lg">
                         Continue Exercise</a>
                 @endif
             </div>
         </div>
     </div>
+</div>
 @endsection
 
 @section('content')
